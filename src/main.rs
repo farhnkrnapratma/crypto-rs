f184444705e5b6d4017c1162eca50592d0997632
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-04-27 05:45:15 +0700

    Patch: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use caesar::encrypt;
+
+fn main() {
+    println!("{}", encrypt("Hello, World!"));
+}
---
+++ vigenere/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn encrypt(plaintext: &str) -> &str {
+    plaintext
+}
+
+pub fn decrypt(ciphertext: &str) -> &str {
+    ciphertext
+}
+
+/*#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}*/
